32de4f601062ee1808af5af721b447219c6605ef
camarguinho <linkcamargo@gmail.com>
2026-02-05 22:39:45 -0300

    aprendendo predicate

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * Function\<T, R> → recebe um T, devolve um R
@@ -10,17 +11,31 @@ import java.util.function.Function;
 public class Main {
 
     public static void main(String[] args){
-        learningFunction();
+        //learningFunction();
+        learningPredicate();
     }
 
     /**
      * Function<T, R> é: me dá um T, eu te devolvo um R.
      * É usada para transformar valores—mapear um tipo em outro.
      */
-    private static void learningFunction() {
+    private static void learningFunction(){
         List<String> input = List.of("a", "b", "c");
         Function<String,String> toUpCase = s -> s.toUpperCase();
         List<String> output = input.stream().map(toUpCase).toList();
         System.out.println(output);
+    }
+
+    /**
+     * Predicate<T> é: me dá um T, eu te digo se é verdadeiro ou falso.
+     * É usado para filtrar, validar, decidir.
+     */
+    private static void learningPredicate(){
+        List<String> input = List.of("Gato", "Pato", "Cachorro");
+        Predicate<String> startWithG = s -> s.startsWith("G");
+        List<String> output = input.stream().filter(startWithG).toList();
+        System.out.println(output);
+
+
     }
 }
